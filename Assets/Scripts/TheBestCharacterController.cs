782c489ed0baa7db0f12082ebcf37f3001f25c09
Brian O'Connor <brian.oconnor@unity3d.com>
2025-01-18 23:15:29 +0000

    Added a Rigidbody to the Character Controller. Press Space to Jump and don't forget to keep building the track!

--- Assets/Scripts/TheBestCharacterController.cs
+++ Assets/Scripts/TheBestCharacterController.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TheBestCharacterController : MonoBehaviour
 {
     public float speed = 1f;
+    public float jumpSpeed = 1f;
+
+    public InputAction jumpAction;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        jumpAction.Enable();
+    }
 
     void Update()
     {
-        this.transform.position += (Vector3.right * speed * Time.deltaTime);
+        rb.MovePosition(rb.position + (Vector3.right * speed * Time.deltaTime));
+
+        if (jumpAction.triggered)
+        {
+            // Must check if we are grounded.
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+        }
     }
 }
