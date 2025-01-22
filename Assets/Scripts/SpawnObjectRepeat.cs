ce510485874f83c9cc3fd2503df020756fde7d51
gavinkileyunity <138685152+gavinkileyunity@users.noreply.github.com>
2025-01-22 16:50:45 +0000

    Updates Gavin scene to spawn water and background prefabs repeatedly

---
+++ Assets/Scripts/SpawnObjectRepeat.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SpawnObjectRepeat : MonoBehaviour
+{
+    public GameObject firstObject;
+    EndingAnchor lastSegmentEndAnchor;
+    public GameObject objectToPlace;
+
+    private void Awake()
+    {
+        if (firstObject == null)
+            Debug.LogError("Have not set initialSegment!");
+
+        lastSegmentEndAnchor = firstObject.GetComponentInChildren<EndingAnchor>(); // We have the initial segment at 0.
+    }
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 5.0f, 5.0f);
+    }
+
+    public void SpawnObject()
+    {
+        Spawn(objectToPlace, lastSegmentEndAnchor);
+    }
+
+    void Spawn(GameObject prefab, EndingAnchor existingSegmentAnchor)
+    {
+        GameObject newSegment = Object.Instantiate(prefab); // A clever person would make this InstantiateAsync :eyes:
+
+        newSegment.transform.position = existingSegmentAnchor.transform.position;
+        lastSegmentEndAnchor = newSegment.GetComponentInChildren<EndingAnchor>();
+    }
+}
